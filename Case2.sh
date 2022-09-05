153b33a66bcc3dbca7a1f1a6e7bded64c889c9f2
FalconLKy <93296964+FalconLKy@users.noreply.github.com>
2022-09-04 21:19:17 -0300

    Update Case2.sh

--- Case2.sh
+++ Case2.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-my_input="/home/kali/Documents/case2/Planilha-case2.csv"
+my_input="/home/kali/Documents/case2/Planilha-case2.csv"     #adicionar local do arquivo .csv
 declare -a u
 declare -a e
-declare -a s
 declare -a se
-while IFS=, read -r usuario email senha setor;
+declare -a s
+#declaro as variaveis 
+while IFS=, read -r usuario email setor senha;       #aqui faço um separador por "," usando o IFS 
 do
     u+=("$usuario")
     e+=("$email")
-    s+=("$senha")
     se+=("$setor")
+    s+=("$senha")
+    #adiciono um valor do arquivo .csv para cada variavel
 done<$my_input
 
 for index in "${!u[@]}";
@@ -19,5 +21,6 @@ do
     sudo useradd -g "${se[$index]}" \
                  -d "/home/${u[$index]}" \
                  -s "/bin/bash" \
-                 -p "$(echo "${s[$index]}" | openssl passwd -1 -stdin)" "${u[$index]}"
+                 -p "$(echo "${s[$index]}" | openssl passwd -1 -stdin)" "${u[$index]}"       #encripto a senha utilizando openssl            
+                 #crio usuario e grupo, e adiciono ao diretorio /home e ao /bin/bash
 done
